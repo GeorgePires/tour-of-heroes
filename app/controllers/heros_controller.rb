13e615f57d87c671d1fb95ec27bf01d630dbe0db
George <georgeasp.pi@gmail.com>
2021-10-24 17:22:12 -0300

    :books: org script

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -1,34 +1,30 @@
 class HerosController < ApplicationController
-#? Solic senha de usuario e criando senha de acesso no master.key
-#! Run EDITOR="code --wait" bin/rails credentials:edit
+  # ? Solic senha de usuario e criando senha de acesso no master.key
+  # ! Run EDITOR="code --wait" bin/rails credentials:edit
 
-  http_basic_authenticate_with name: Rails.application.credentials.authenticate[:name], 
+  http_basic_authenticate_with name: Rails.application.credentials.authenticate[:name],
                                password: Rails.application.credentials.authenticate[:password],
-                               except: [:index, :show]
+                               except: %i[index show]
 
-  before_action :set_hero, only: %i[ show edit update destroy ]
+  before_action :set_hero, only: %i[show edit update destroy]
 
-  
   def index
     @heros = Hero.all
   end
-  
-  def show
-  end
-  
+
+  def show; end
+
   def new
     @hero = Hero.new
   end
 
-  def edit
-  end
+  def edit; end
 
   def create
     @hero = Hero.new(hero_params)
-
     respond_to do |format|
       if @hero.save
-        format.html { redirect_to @hero, notice: "Hero was successfully created." }
+        format.html { redirect_to @hero, notice: 'Hero was successfully created.' }
         format.json { render :show, status: :created, location: @hero }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -37,11 +33,13 @@ class HerosController < ApplicationController
     end
   end
 
+
+
   # PATCH/PUT /heros/1 or /heros/1.json
   def update
     respond_to do |format|
       if @hero.update(hero_params)
-        format.html { redirect_to @hero, notice: "Hero was successfully updated." }
+        format.html { redirect_to @hero, notice: 'Hero was successfully updated.' }
         format.json { render :show, status: :ok, location: @hero }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -50,23 +48,24 @@ class HerosController < ApplicationController
     end
   end
 
-  # DELETE /heros/1 or /heros/1.json
+
+
+  # DELETE 
   def destroy
     @hero.destroy
     respond_to do |format|
-      format.html { redirect_to heros_url, notice: "Hero was successfully destroyed." }
+      format.html { redirect_to heros_url, notice: 'Hero was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_hero
-      @hero = Hero.find(params[:id])
-    end
+ # Use callbacks para compartilhar configurações ou restrições comuns entre as ações.
+  def set_hero
+    @hero = Hero.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def hero_params
-      params.require(:hero).permit(:name)
-    end
+  def hero_params
+    params.require(:hero).permit(:name)
+  end
 end
--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -4,4 +4,6 @@ class ApplicationJob < ActiveJob::Base
 
   # Most jobs are safe to ignore if the underlying records are no longer available
   # discard_on ActiveJob::DeserializationError
+  #? Criar Jobs>>> Ex: Roda em segundo plano
+  #! Enviar email no back para user 
 end
